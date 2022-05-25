284c853e56e6b02ff3dde87d4467df246af41f4c
Everton000 <evertonmartins000@hotmail.com>
2022-05-24 21:20:40 -0300

    Relacionamento cinema e filme (sessoes)

---
+++ Migrations/20220524230324_Adicionando relacionamento entre cinema e filme.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmesAPI.Migrations
+{
+    public partial class Adicionandorelacionamentoentrecinemaefilme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CinemaFilme",
+                columns: table => new
+                {
+                    CinemasId = table.Column<int>(type: "int", nullable: false),
+                    FilmesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CinemaFilme", x => new { x.CinemasId, x.FilmesId });
+                    table.ForeignKey(
+                        name: "FK_CinemaFilme_Cinemas_CinemasId",
+                        column: x => x.CinemasId,
+                        principalTable: "Cinemas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CinemaFilme_Filmes_FilmesId",
+                        column: x => x.FilmesId,
+                        principalTable: "Filmes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CinemaFilme_FilmesId",
+                table: "CinemaFilme",
+                column: "FilmesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CinemaFilme");
+        }
+    }
+}
--- Models/Cinema.cs
+++ Models/Cinema.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
@@ -14,5 +15,7 @@ namespace FilmesAPI.Models
         public int EnderecoId { get; set; }
         public virtual Gerente Gerente { get; set; }
         public int GerenteId { get; set; }
+        [JsonIgnore]
+        public virtual List<Sessao> Sessoes { get; set; }
     }
 }
